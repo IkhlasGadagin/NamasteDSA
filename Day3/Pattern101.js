a16b48b32e6bd3ab48f62315d1b6139b7553ea1b
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-06-26 13:53:29 +0530

    pattern 10 toggle

---
+++ Day3/Pattern101.js
@@ -0,0 +1,15 @@
+const n = 5;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    let toggle = 1;
+    for (let j = 0; j < i + 1; j++) {
+        row = row + toggle;
+        if (toggle == 0) {
+            toggle = 1;
+        }
+        else {
+            toggle = 0;
+        }
+    }
+    console.log(row)
+}
