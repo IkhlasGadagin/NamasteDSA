69aba29085549bdd9ad9dc853429b70471f53c00
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-06-27 19:32:04 +0530

    the reverse number is returned

--- Day4/reverseInteger.js
+++ Day4/reverseInteger.js
@@ -1,15 +1,21 @@
 //write a program to reverse a number in and handle the limitation
 function reverseNumber(num) {
-
     let rev = 0;
+    const original = num;
     num = Math.abs(num);
     while (num > 0) {
         let rem = num % 10
         num = Math.floor(num / 10);
         rev = (rev * 10) + rem;
     }
-    return rev;
+
+    const limit = Math.pow(2, 31);
+    if (rev > limit || rev < -limit) return 0
+
+    return (original < 0) ? -rev : rev;
+
+
 }
-const num = -1223234;
+const num = -823234;
 const ans = reverseNumber(num);
 console.log(ans);
