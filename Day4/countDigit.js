d8f4858a92ec3c44ef9c08dd460eb93f12418bb5
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-06-27 11:14:14 +0530

    the number of the count digit is counted

--- Day4/countDigit.js
+++ Day4/countDigit.js
@@ -3,6 +3,7 @@ function countDigit(num) {
     if (num == 0) {
         return 1
     }
+    num = Math.abs(num);
     let count = 0;
     while (num > 0) {
         num = Math.floor(num / 10)
@@ -11,6 +12,6 @@ function countDigit(num) {
     return count
 }
 
-const num = 124212;
+const num = -10;
 const ans = countDigit(num);
 console.log(ans);
