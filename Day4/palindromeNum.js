4f9b7b6ecdff4af40fcd18ef98bc1e9a9905563f
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-06-27 12:29:16 +0530

    the palindrome logic is done

---
+++ Day4/palindromeNum.js
@@ -0,0 +1,20 @@
+function palindrom(num) {
+    let copy = num;
+    let rev = 0;
+    while (num > 0) {
+        let remainder = num % 10
+        rev = (10 * rev) + remainder;
+        num = Math.floor(num / 10);
+    }
+    if (rev === copy) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+const number = 124441;
+const ans = palindrom(number)
+console.log(ans);
+
