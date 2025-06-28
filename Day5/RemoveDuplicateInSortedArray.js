9624cdccb546e19e48e09fbbc11ac54a4079f152
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-06-28 11:36:07 +0530

    the remove the duplicate the array with the pointer

--- Day5/RemoveDuplicateInSortedArray.js
+++ Day5/RemoveDuplicateInSortedArray.js
@@ -13,7 +13,7 @@ function removeDup(arr) {
 
 }
 
-const array = [0, 0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 9]
+const array = [0, 1, 1, 1, 2, 2, 3, 3, 4]
 const ans = removeDup(array)
 console.log(ans);
 
