bf40cb572d488e002979a258006840567b37f4cf
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-02 20:42:06 +0530

    1111count

---
+++ Day9/countConsigetive.js
@@ -0,0 +1,21 @@
+function countCon(arr) {
+    let count = 0
+    let maxCount = 0
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] == 1) {
+            count++
+        }
+        else {
+            maxNumber = Math.max(count, maxCount);
+            count = 0
+        }
+
+    }
+
+    return maxNumber;
+}
+
+const arr = [1, 1, 0, 1, 1, 1];
+const ans = countCon(arr);
+console.log(ans);
