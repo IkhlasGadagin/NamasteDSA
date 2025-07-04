94105a8c13bdcd1994383a69ff38cf7eac1c975e
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-04 17:04:23 +0530

    Recur

--- Day10/Recursion1.js
+++ Day10/Recursion1.js
@@ -1,11 +1,23 @@
-function Rec(num) {
-    if (num == 0) return
+// function Rec(num) {
+//     if (num == 0) return
 
-    console.log(num);
-    num = num - 1;
-    Rec(num)
+//     console.log(num);
+//     num = num - 1;
+//     Rec(num)
+
+// }
+
+// const num = 100;
+// Rec(num);
+
+//print 1 to n numners
+
+function Rec3(x) {
+    if (x == num) return;
+    console.log(x);
+    x = x + 1;
+    Rec3(x)
 
 }
-
-const num = 100;
-Rec(num);
+const num = 114;
+Rec3(1)
