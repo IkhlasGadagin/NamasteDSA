b3b95d7247f03f081b9cf6d16e940b83a1ab90ce
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-04 18:37:59 +0530

    the sum of Evens in array

---
+++ Day11/SumOfAllEven.js
@@ -0,0 +1,13 @@
+function sumEven(n) {
+    if (n == 0) return 0;
+    if (arr[n] % 2 == 0) {
+        return arr[n] + sumEven(n - 1)
+    }
+    else {
+        return sumEven(n - 1);
+    }
+}
+
+const arr = [1, 2, 3, 4, 5, 4, 5, 3, 7, 8, 5, 11];
+const ans = sumEven(arr.length - 1);
+console.log(ans);
