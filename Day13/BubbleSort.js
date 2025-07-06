883de36d9886af68485e02b1af1cf5123314b924
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-06 13:53:42 +0530

    BUBBLE SORT

---
+++ Day13/BubbleSort.js
@@ -0,0 +1,24 @@
+/* the bubble sort involves two loop i ans j with the same referesnce
+   the first iteration gives the last element correct 
+   the second give the second bigger element at the last second position
+   so iterate only -i
+   swap
+ */
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j + 1] < arr[j]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+//if (arr[j] > arr[j + 1])  or arr[j+1]<arr[j]
+
+const array = [4, 5, 3, 2, 5, 3, 6, 34, 2];
+const ans = bubbleSort(array);
+console.log(ans);
