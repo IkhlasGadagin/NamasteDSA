15720ce149ec198bbeb20bfec4bed25fb969b623
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-07 03:41:37 +0530

    the bubble sort improved

--- Day13/BubbleSort.js
+++ Day13/BubbleSort.js
@@ -7,11 +7,14 @@
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - 1 - i; j++) {
+            let isSwapped = false;
             if (arr[j + 1] < arr[j]) {
                 let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                isSwapped = true
             }
+            if (!isSwapped) break;
         }
     }
     return arr;
