54132c8016d46135347191a12345e29a5e8641c1
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-07 16:55:37 +0530

    done with the selection sort day 14

--- Day14/SelectionSort.js
+++ Day14/SelectionSort.js
@@ -1,12 +1,17 @@
 function selectionSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-        let min = arr[i];
+        let min = i;
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[min]) {
-                arr[min] = arr[j];
+                min = j;
 
             }
         }
+        //swap arr[i] = arr[min]
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+
     }
     return arr;
 }
