dcf60947b9a4a653086797f30f35aea52ac55a8e
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-09 02:28:01 +0530

    te merge

--- Day15/MergeSort.js
+++ Day15/MergeSort.js
@@ -1,23 +1,13 @@
 //Devide and conquere rule bu recursion return the sorted by helper function to the caller
-
-// function mergeSort(arr) {
-
-//     if (arr.length <= 0) return arr;
-
-//     let mid = Math.floor(arr.length / 2);
-//     let left = mergeSort(arr.slice(0, mid));
-//     let right = mergeSort(arr.slice(mid));
-//     return sortingHelper(left, right);
-
-// }
 function mergeSort(arr) {
-    if (arr.length <= 1) return arr;
+
+    if (arr.length <= 0) return arr;
 
     let mid = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, mid));
     let right = mergeSort(arr.slice(mid));
-
     return sortingHelper(left, right);
+
 }
 
 function sortingHelper(arr1, arr2) {
@@ -37,8 +27,6 @@ function sortingHelper(arr1, arr2) {
     }
     return [...ans, ...arr1.slice(i), ...arr2.slice(j)];
 }
-
 const array = [3, 4, 5, 2, 3, 5, 7, 6, 8, 9];
-
 const ans = mergeSort(array);
 console.log(ans);
