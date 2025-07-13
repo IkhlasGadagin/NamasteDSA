813f6f3676bbddbd6a7a5c01f2a905e21461b95a
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-14 03:15:59 +0530

    fast ans slow tried got middle

--- Day20/FastAnsSlow.js
+++ Day20/FastAnsSlow.js
@@ -1,15 +1,20 @@
 function middleNode(head) {
-    let slow = this.head;
-    let fast = this.head;
-    for (let i = 0; i < size - 1; i++) {
-        slow = slow.next;
-        fast = slow.next.next;
-        if (fast.next == null) {          //odd number digits
-            return slow;
-        }
-        else if (fast.next.next == null) {
-            return slow.next.next         //even numbers 
+    if (head.next == null) return head.val;
+    if (head.next.next == null) return head.next.val;
+    else {
+        let slow = this.head;
+        let fast = this.head;
+        for (let i = 0; i < size - 1; i++) {
+            slow = slow.next;
+            fast = slow.next.next;
+            if (fast.next == null) {          //odd number digits
+                return slow;
+            }
+            else if (fast.next.next == null) {
+                return slow.next.next         //even numbers 
+            }
         }
     }
+
 }
 middleNode(this.head);
