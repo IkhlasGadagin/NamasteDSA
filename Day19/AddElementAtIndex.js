757df5c7dcb5fa9f2ae892ab4571382f6e8bc09b
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-13 18:50:23 +0530

    cornrer case

--- Day19/AddElementAtIndex.js
+++ Day19/AddElementAtIndex.js
@@ -11,23 +11,27 @@ function addElement(val, index) {
     }
 
     else if (index == this.size) {
-        let curr = this.head;
-        while (curr.next != null) {
+        this.AddElementLast(val);
+        /*  let curr = this.head;
+         while (curr.next != null) {
+             cur = cur.next;
+         }
+         cur.next = newNode;
+         this.size++; */
+    }
+
+
+    else {
+        const cur = this.head;
+        for (let i = 0; i < index - 1; i++) {
             cur = cur.next;
         }
+        newNode.next = cur.next;
         cur.next = newNode;
-        this.size++;
     }
-
-
-    const cur = this.head;
-    for (let i = 0; i < index - 1; i++) {
-        cur = cur.next;
-    }
-    newNode.next = cur.next;
-    cur.next = newNode;
 
 }
+
 const element = 11;
 const index = 1;
 const ans = addElement(element, index);
