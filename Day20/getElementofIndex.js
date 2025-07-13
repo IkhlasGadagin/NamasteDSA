43ce16550752b84df5eac22df6f26f5f6c378ca7
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-13 22:46:26 +0530

    getElement at given index???

---
+++ Day20/getElementofIndex.js
@@ -0,0 +1,18 @@
+function getElementofIndex(index) {
+    if (this.size == 0) return -1
+
+    if (index == 0) return this.head.val;
+    // if(index ==size) return 
+
+    // while(curr.next!=null)
+    let curr = this.head;
+
+    for (let i = 0; i < index; i++) {
+        curr = curr.next;
+
+    }
+    return curr.val;
+}
+
+const index = 5;
+getElementofIndex(index)
