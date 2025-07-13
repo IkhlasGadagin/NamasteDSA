bf41a808783420a84d5528a957f7759bb9d7774b
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-14 00:07:26 +0530

    delete the node

--- Day20/DeleteElementofIndex.js
+++ Day20/DeleteElementofIndex.js
@@ -1,18 +1,20 @@
 function DeleteElementofIndex(index) {
-    if (index < 0) return -1;
+    if (index < 0 || index >= size) return;
 
     if (index === 0) {
         this.head = this.head.next;
         size--;
     }
-    let cur = this.head;
-    while (cur.next != null) {
+    else {
+        let cur = this.head;
         for (let i = 0; i < index - 1; i++) {
             cur = cur.next;
         }
+
+        cur.next = cur.next.next;
+        this.size--;
     }
-    cur.next = cur.next.next;
-    this.size--;
+
 }
 
 const val = 5;
