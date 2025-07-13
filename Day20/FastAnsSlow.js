417ead3349b9effeb0aa0922e5d95258f4a95a7f
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-14 04:19:40 +0530

    while modified 2 pointer

--- Day20/FastAnsSlow.js
+++ Day20/FastAnsSlow.js
@@ -21,7 +21,7 @@
 function FastAndSlow(head) {
     let slow = head;
     let fast = head;
-    while (fast != null && fast.next != null) {      //for even numbers fast becomes null at the last sec
+    while (fast && fast.next) {  //for even numbers fast becomes null at the last sec
         //for odd numbers fast.next becomes null at the end
         slow = slow.next;
         fast = fast.next.next;
