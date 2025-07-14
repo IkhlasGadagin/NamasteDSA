f20596d94b8c8ba6aa98b1adce6fbfe4ccb2678d
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-14 23:10:06 +0530

    the cyclic list is identified by two pointer first try success

---
+++ Day22/CyclePointer.js
@@ -0,0 +1,14 @@
+function CyclePointer(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+}
+
+CyclePointer(this.head);
