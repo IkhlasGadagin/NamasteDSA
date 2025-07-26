952545a844cab1e68a881f297a124d113a28354f
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-26 13:20:15 +0530

    the array index is retuened

--- Day33/JewelsStonesSet.js
+++ Day33/JewelsStonesSet.js
@@ -1,4 +1,5 @@
 function JewelsStonesSet(jewels, stones) {
+    let ans = [];
     let set = new Set();
     for (let i = 0; i < jewels.length; i++) {
         set.add(jewels[i]);
@@ -7,10 +8,10 @@ function JewelsStonesSet(jewels, stones) {
     let count = 0;
     for (let i = 0; i < stones.length; i++) {
         if (set.has(stones[i])) {
-            count++
+            ans.push(i)
         }
     }
-    return count;
+    return ans;
 }
 
 const jewels = "Aa";
