f2efcf4a87e8bfc52dd5de9009d68bdea3ba6b6a
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-07-30 21:46:38 +0530

    the highest prefix is done

---
+++ Day37/preFiX.js
@@ -0,0 +1,17 @@
+function preFiX(strs) {
+    let x = 0;
+    while (x < strs[0].length) {
+        let char = strs[0][x];
+        for (let i = 1; i < strs.length; i++) {
+            if (char !== strs[i][x] || x === strs[i].length) {
+                return strs[0].substring(0, x)
+            }
+        }
+        ++x;
+    }
+    return strs[0];
+}
+
+const strs = ["flower", "flow", "flight"];
+const ans = preFiX(strs);
+console.log(ans);
