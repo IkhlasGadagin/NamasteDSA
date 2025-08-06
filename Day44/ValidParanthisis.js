9d0f3a912aa6279601eec3a97b4bb2933e59788b
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-06 22:00:57 +0530

    the valid Paranthisis is done and only one test case is failed

---
+++ Day44/ValidParanthisis.js
@@ -0,0 +1,27 @@
+function validParanthisis(para) {
+    let par = para.split('');
+    let map = {
+        "}": "{",
+        ")": "(",
+        "]": "["
+    }
+    // console.log(par, map["{"]);
+    let openBraces = ["(", "{", "["];
+    let stack = [];
+    for (let i = 0; i < par.length; i++) {
+        if (openBraces.includes(par[i])) {
+            stack.push(par[i])
+        }
+        else if (map[par[i]] === stack[stack.length - 1]) {
+            stack.pop()
+        }
+    }
+    if (stack.length === 0) {
+        return true;
+    }
+    else return false;
+
+}
+const paran = "[]()[{()}]{}";
+const ans = validParanthisis(paran);
+console.log(ans);
