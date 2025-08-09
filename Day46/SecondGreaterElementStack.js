33fe8458a420d7464538523d512acbeca308e48d
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-09 20:35:42 +0530

    the algorithm to find the second largest by stack

---
+++ Day46/SecondGreaterElementStack.js
@@ -0,0 +1,8 @@
+/* Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1] */
+//num2 has to be iteaate to find the second largest
+//Start from the last end
+//two condition current element is smaller that the next that it correct map it
+//else that is the the current is smaller that the next
+//for this keep pushing
+//while loop for to find the second largest from the stack pop until to find
