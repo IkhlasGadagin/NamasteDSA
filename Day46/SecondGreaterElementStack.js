6da4de7214c69d2e66533adf0ee2f223866ae63c
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-10 12:50:53 +0530

    the nextGreater  is  coded successfully

--- Day46/SecondGreaterElementStack.js
+++ Day46/SecondGreaterElementStack.js
@@ -8,18 +8,53 @@ Output: [-1,3,-1] */
 //while loop for to find the second largest from the stack pop until to find
 
 function SecondLargest(nums, arr) {
+    //creat an object to map the perticular element to largest of second
     let map = {};
+    //create stack
     let stack = [];
-    let lastElement = arr[arr.length - 1];
+    //the last element's greater not exist  so map to -1
+    let n = arr.length;
+    stack.push(arr[n - 1]);
 
-    map[lastElement] = -1;
-    stack.push(lastElement);
-    console.log(map, stack);
+    //push last elment to stack
+    map[arr[n - 1]] = -1;
+    //loop starts from the last second element
+    for (let i = n - 2; i >= 0; i--) {
+        //if the currnt element is is smaller that the top of the stack means
+        //map current to the top element in {}
+        //if current element is smaller that the top
+        //iterate until length and find the largest in the stack if found break
+        //if not found map cur element to -1 
+        //keep pushing the curret to the stack doesn' matter smaller 
+        if (arr[i] < stack[stack.length - 1]) {
+            map[arr[i]] = stack[stack.length - 1]
+        }
+        else {
+            while (stack.length) {
+                if (stack[stack.length - 1] < arr[i]) {
+                    stack.pop()
+                }
+                else {
+                    map[arr[i]] = stack[stack.length - 1];
+                    break;
+                }
 
+            }
+            if (stack.length === 0) {
+                map[arr[i]] = -1;
+            }
+        }
+        stack.push(arr[i])
+    }
 
+    let ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(map[nums[i]]);
+    }
+    return ans;
 }
 let arr1 = [4, 1, 2];
-let arr2 = [4, 1, 2];
+let arr2 = [1, 3, 4, 2];
 let ans = SecondLargest(arr1, arr2);
 console.log(ans);
 
