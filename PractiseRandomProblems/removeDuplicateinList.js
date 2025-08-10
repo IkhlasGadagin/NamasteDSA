09e4af39a8f07bea61f04341b2d7b9b424c402a9
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-11 01:36:52 +0530

    the duplicet is removed and cur.next must exist

---
+++ PractiseRandomProblems/removeDuplicateinList.js
@@ -0,0 +1,12 @@
+function RemoveDuplicate(head) {
+    let cur = head;
+    while (cur && cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        }
+        else {
+            cur = cur.next;
+        }
+    }
+    return head;
+}
