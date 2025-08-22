f1362543f90f7273cb42e2ea502d4ddbce644453
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-22 22:33:24 +0530

    basic teached

---
+++ Revision/array.js
@@ -0,0 +1,16 @@
+let car = ["bmw", "lambo", "farari", "oddi"];
+
+let len = car.length;
+
+
+
+for (let i = 0; i < len; i++) {
+
+    if (car[i] === "lambo") {
+        console.log("lambo present");
+    }
+    else {
+        console.log("not present");
+    }
+
+}
