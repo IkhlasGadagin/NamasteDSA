6eb06182503c325c65b5c5096e459a324bb149b9
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-08-23 00:15:31 +0530

    the teacher 2

---
+++ Revision/oddNUmber.js
@@ -0,0 +1,22 @@
+//write the odd numbers fromm 1 to 1000;
+function oddNumber() {
+    for (let i = 2000; i > 0; i--) {
+        if (i % 2 === 1) {
+            console.log(i);
+        }
+    }
+}
+function evenNumber(arr) {
+    // for (let i = 2000; i > 0; i--) {
+    //     if (i % 2 === 0) {
+    //         console.log(i);
+    //     }
+    // }
+    console.log(arr);
+
+}
+
+const nums = [2, 5, 4, 2, 4]
+
+evenNumber(nums);
+
