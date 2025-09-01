ceeb122a52e458c6687410c134c661ec173776bb
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-09-01 22:51:22 +0530

    he revision

--- Revision2.0/rev.js
+++ Revision2.0/rev.js
@@ -19,6 +19,16 @@ function OuterParanthisis(para) {
     }
     return ans;
 }
-const para = "()()((()))"
-const ans = OuterParanthisis(para);
+
+
+
+function Count(arr) {
+    let map = {};
+    for (let i = 0; i < arr.length; i++) {
+        map[arr[i]] = map[arr[i]] ? ++map[arr[i]] : 1
+    }
+    return map;
+}
+const str = "jhsdafjhsgdjaf";
+const ans = Count(str);
 console.log(ans);
