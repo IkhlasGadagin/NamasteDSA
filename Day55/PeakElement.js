3a53b54b8ade668a851808bcbcf9349177968d88
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-09-18 20:14:11 +0530

    the algo for the peak

---
+++ Day55/PeakElement.js
@@ -0,0 +1,14 @@
+/* 
+the peak element is the element which is
+bigger that the neighbour element 
+solved by the binery search 
+0 to last element 
+the while loop 
+less than l with r
+the mid 
+the mid and the next element is bigger then 
+the next element is the ans
+else 
+move right to the mid
+at last  coinside l ans r
+*/
